d462a02d354035f2f3a2f3a6e0d349d89541a3db
Matthew LaCorte <mlacorte@uncc.edu>
2018-03-02 18:44:10 -0500

    cleaned up code
    cleaned it up to make it look pretty, as well as adding more descriptive comments

--- v1.ino
+++ v1.ino
@@ -1,46 +1,56 @@
-#include <msp430g2553.h>
-#include <pins_energia.h>
+/* * * * * * * * * * * * * * * * * * * *
+ * ECGR 2252 | Sophomore Design        *
+ *                                     *
+ * TEAM 5                              *
+ * Code by Matt LaCorte & Josh Martin  *
+ * * * * * * * * * * * * * * * * * * * */
 
-int motorspeedl = 200;
-int motorspeedr = 200;
+//files to include
+#include <msp430g2553.h>  //added by Energia IDE
+#include <pins_energia.h>  //stored within folder
 
-int blackValR = 965; //black value is high num
-int blackValL = 700; //black value for left sensor
+/*GLOBAL VARS*/
+//motor speeds allow for easy adjustments to speed
+int motorspeedl = 200;  //right motor
+int motorspeedr = 200;  //left motor
 
+//black line threshhold -- BLACK is high -- WHITE is low
+int blackValR = 965; //right sensor threshhold
+int blackValL = 700; //left sensor threshhold
+
+//stores vals read by sensors
 int r = 0; //left sensor val
 int l = 0; //right sensor val
 
+
+//main setup for hardware declarations
 void setup() {
 
-  pinMode(12,OUTPUT); //motor left forward
-  pinMode(13,OUTPUT); //motor left backwards
+  pinMode(12,OUTPUT);  //motor left forward
+  pinMode(13,OUTPUT);  //motor left backward
   pinMode(15,OUTPUT);  //motor right backward
-  pinMode(10,OUTPUT); //motor right forward
+  pinMode(10,OUTPUT);  //motor right forward
 
-  pinMode(8,OUTPUT); //right side motor do some fancy shit
-  pinMode(9,OUTPUT); //left side motor do some fancy shit
+  pinMode(8,OUTPUT); //right motor enable
+  pinMode(9,OUTPUT); //left motor enable
   
   pinMode(14,OUTPUT); //green led
   pinMode(2,OUTPUT);  //red led
   
   pinMode(7,INPUT);   //left sensor
   pinMode(6,INPUT);   //right sensor
+  
   Serial.begin(9600);
+  
   delay(1000);
-
 }
 
-void loop() {
-//  for(int i =0; i <5;i++){
-//    digitalWrite(2,HIGH);
-//    digitalWrite(14,HIGH);
-//    delay(500);
-//    digitalWrite(2,LOW);
-//    digitalWrite(14,LOW);
-//    delay(500);
-//  }
 
-  if ((l < blackValL) && (r < blackValR)) { //Go forward (WW)
+//main loop to run through the track
+void loop() {
+  
+  //WHITE WHITE -- drive forwards
+  if ((l < blackValL) && (r < blackValR)) {
       digitalWrite(2,LOW);
       digitalWrite(14,LOW);
       
@@ -51,7 +61,8 @@ void loop() {
       analogWrite(10, motorspeedr);
   }
   
-    else if ((l >= blackValL) && (r < blackValR)) { //Left Turn (BW)
+    //BLACK WHITE -- turn right
+    else if ((l >= blackValL) && (r < blackValR)) {
       digitalWrite(2,HIGH);
       digitalWrite(14,LOW);
 
@@ -61,6 +72,7 @@ void loop() {
       analogWrite(10, motorspeedr + 65);         
     }
   
+    //WHITE BLACK -- turn right
     else if ((l < blackValL) && (r >= blackValR)) { //Right Turn (WB)
 
       digitalWrite(2,LOW);
@@ -72,6 +84,7 @@ void loop() {
       analogWrite(12, motorspeedl + 65);
     }
   
+    //BLACK BLACK -- turn left 90 degrees
     else if ((l >= blackValL) && (r >= blackValR)) { //Left Turn (BB)
         digitalWrite(2,HIGH);
         digitalWrite(14,HIGH);
@@ -79,47 +92,27 @@ void loop() {
         digitalWrite(8,HIGH);
         digitalWrite(9,HIGH);
         
-      
         analogWrite(12, 0);
         analogWrite(13, 0);
         analogWrite(10, 0);
         analogWrite(15, 0);
+      
         delay(2000);
 
         analogWrite(10, motorspeedr);
+      
         delay(2000);
-
-        //while (r < blackValR){
-          //analogWrite(10, motorspeedr);
-          //r = analogRead(6);
-       // }
     }
     
+  //NOTHING
     else {
-      
-     for(int i = 0; i < 10; i++) {
-        digitalWrite(2,HIGH);
-        digitalWrite(14,LOW);
-        delay(250);
-        digitalWrite(2,LOW);
-        digitalWrite(14,HIGH);
-        delay(250);
-      }
-      digitalWrite(8,LOW);
-      digitalWrite(9,LOW);
-      
-      analogWrite(12, 0);
-      analogWrite(13, 0);
-      analogWrite(10, 0);
-      analogWrite(15, 0);
-      
+      //pray
     }
 
   l = analogRead(7); //left sensor val
   r = analogRead(6); //right sensor val
 
-  Serial.println(r);
+  Serial.print(r);
   Serial.print("  |  ");
-  Serial.print(l);
-  
+  Serial.println(l);
 }
